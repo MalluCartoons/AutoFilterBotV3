a5e275710baa8ebb1ad48a36aaf2282840c51409
MalluCartoons <92737001+MalluCartoons@users.noreply.github.com>
2021-10-19 14:48:49 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -112,7 +112,7 @@ async def start(bot, message):
         await message.reply_photo(
         "https://te.legra.ph/file/1064929a142a439a27f0f.jpg",
         caption=START_MSG.format(message.from_user.first_name),
-        reply_markup=InlineKeyboardMarkup,
+        reply_markup=InlineKeyboardMarkup)
         (
                 [[
                 InlineKeyboardButton("Search Here", switch_inline_query_current_chat='')
